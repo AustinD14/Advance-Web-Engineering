40ac5cabd5c222fd64a5fd7f310b1ac79a7e65cf
AustinD14 <austindimalanta14@gmail.com>
2020-02-19 13:10:24 -0800

    fixing userBrowser

--- hw2.js
+++ hw2.js
@@ -11,24 +11,52 @@ function addEmployee(){
   var department = document.getElementById('department').value;
   var employeeID = Math.floor(Math.random() * 100000000) + 1;
   var employee = {
-    firstname: firstname,
-    lastname: lastname,
-    department: department,
-    employeeID: employeeID,
-    date: date
+    "firstname": firstname,
+    "lastname": lastname,
+    "department": department,
+    "employeeID": employeeID,
+    "date": date
   };
-  
+
+
   Employees.push(employee);
   console.log(Employees);
-  
+  // console.log(JSON.stringify(Employees));
+
+
   document.getElementById('outputname').innerHTML ="Name: " + lastname + ", " + firstname;
   document.getElementById('outputdepartment').innerHTML ="Department: " + department;
   document.getElementById('outputemployeeid').innerHTML ="Employee ID: " + employeeID;
   document.getElementById('outputhiredate').innerHTML ="Hire Date: " + date;
   document.getElementById('outputtotalemployees').innerHTML ="Total Employees: " + Employees.length;
-  document.getElementById('browser').innerHTML ="Browser: " + navigator.appName;
+  document.getElementById('browser').innerHTML ="Browser: "+ currentBrowser();
 
   return false;
+}
+
+function currentBrowser(){
+  var sBrowser, sUsrAg = navigator.userAgent;
+
+if (sUsrAg.indexOf("Firefox") > -1) {
+  sBrowser = "Mozilla Firefox";
+} else if (sUsrAg.indexOf("SamsungBrowser") > -1) {
+  sBrowser = "Samsung Internet";
+} else if (sUsrAg.indexOf("Opera") > -1 || sUsrAg.indexOf("OPR") > -1) {
+  sBrowser = "Opera";
+} else if (sUsrAg.indexOf("Trident") > -1) {
+  sBrowser = "Microsoft Internet Explorer";
+} else if (sUsrAg.indexOf("Edge") > -1) {
+  sBrowser = "Microsoft Edge";
+} else if (sUsrAg.indexOf("Chrome") > -1) {
+  sBrowser = "Google Chrome or Chromium";
+} else if (sUsrAg.indexOf("Safari") > -1) {
+  sBrowser = "Apple Safari";
+} else {
+  sBrowser = "unknown";
+}
+
+  // alert("You are using: " + sBrowser);
+  return sBrowser;
 }
 
 function init() {
